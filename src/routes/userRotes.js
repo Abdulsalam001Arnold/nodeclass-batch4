c8743d23656e83064b3a58c4b0aadab52f130be4
Abdulsalam001Arnold <abdulsalamabulhamidarnold@gmail.com>
2025-12-10 13:25:50 +0100

    feat: created signup route

--- src/routes/userRotes.js
+++ src/routes/userRotes.js
@@ -1,12 +1,12 @@
 
 
 import { Router } from "express";
-import { getHome, getAbout } from "../controllers/userControllers.js";
+import { getHome, getAbout, createUser } from "../controllers/userControllers.js";
 
 
 const router = Router()
 
-router.get('/', getHome).get('/about', getAbout)
+router.get('/', getHome).get('/about', getAbout).post('/signup', createUser)
 
 
 export default router;
